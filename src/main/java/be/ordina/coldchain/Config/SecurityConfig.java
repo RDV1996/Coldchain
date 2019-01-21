17b078e74f24687455191d6de91670e4141f8c7f
Unknown <roby1996_10@hotmail.com>
2019-01-21 18:28:25 +0100

    Add recaurse propperties back in

--- src/main/java/be/ordina/coldchain/Config/SecurityConfig.java
+++ src/main/java/be/ordina/coldchain/Config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package be.ordina.coldchain.Config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
